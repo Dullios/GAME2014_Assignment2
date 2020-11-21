6194b10a6a2e2135abdedebc9efb5d645820c4d4
Russell Brabers <brabers.russell@gmail.com>
2020-11-21 13:32:25 -0500

    Updated Title Scene
    - Added title scene features
    - Created button function

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void ChangeScene(int i)
+    {
+        SceneManager.LoadScene(i);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
